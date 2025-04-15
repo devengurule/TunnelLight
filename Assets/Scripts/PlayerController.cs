6456bd2481be3d893271b4f94d56b7730222b663
devengurule <dgurule3@uccs.edu>
2025-04-14 22:23:37 -0600

    Started work on the train stop

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,13 +36,8 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
-        
-
-
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-
-
 
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
 
