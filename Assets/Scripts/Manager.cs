0962f8f4c16fd973ca5dbbc8fe8b0d0173c3dd0c
devengurule <dgurule3@uccs.edu>
2025-04-22 11:55:49 -0600

    Stuff

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -6,5 +6,17 @@ public class Manager : MonoBehaviour
     void Update()
     {
         if(Input.GetKey(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            if(SceneManager.GetActiveScene().name == "TrainStop")
+            {
+                SceneManager.LoadScene("TheTunnel");
+            }
+            else if(SceneManager.GetActiveScene().name == "TheTunnel")
+            {
+                SceneManager.LoadScene("TrainStop");
+            }
+        }
     }
 }
